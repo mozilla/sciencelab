dd46d67732e56e1caef8aa6bfa183f44723f4de0
Ubuntu <ubuntu@domU-12-31-39-14-1A-E5.compute-1.internal>
2014-01-14 01:23:39 +0000

    fix merge conflict

--- themes/vantage-child/header.php
+++ themes/vantage-child/header.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /**
  * The Header for our theme.
@@ -47,54 +46,4 @@
 
 	<div id="main" class="site-main">
 		<div class="full-container">
-=======
-<?php
-/**
- * The Header for our theme.
- *
- * Displays all of the <head> section and everything up till <div id="main">
- *
- * @package vantage
- * @since vantage 1.0
- * @license GPL 2.0
- */
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta http-equiv="X-UA-Compatible" content="IE=10" />
-	<title><?php wp_title( '|', true, 'right' ); ?></title>
-	<link rel="profile" href="http://gmpg.org/xfn/11" />
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	<?php wp_head(); ?>
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	  ga('create', 'UA-35433268-51', 'mozillascience.org');
-	  ga('send', 'pageview');
-	</script>
-</head>
-
-<body <?php body_class(); ?>>
-
-<?php do_action('vantage_before_page_wrapper') ?>
-
-<div id="page-wrapper">
-
-	<?php do_action( 'vantage_before_masthead' ); ?>
-
-	<?php get_template_part( 'parts/masthead', apply_filters( 'vantage_masthead_type', '' ) ); ?>
-
-	<?php do_action( 'vantage_after_masthead' ); ?>
-
-	<?php vantage_render_slider() ?>
-
-	<?php do_action( 'vantage_before_main_container' ); ?>
-
-	<div id="main" class="site-main">
-		<div class="full-container">
->>>>>>> 4d97f8025f92788c2d602b39d6c1a1cb07b03a01
 			<?php do_action( 'vantage_main_top' ); ?>
